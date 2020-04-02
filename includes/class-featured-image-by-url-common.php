2f1d1643dab911cebe7b80ec1238395eb1a058af
Dharmesh Patel <dspatel44@gmail.com>
2020-04-03 00:01:35 +0530

    Made fake attachment id integer

--- includes/class-featured-image-by-url-common.php
+++ includes/class-featured-image-by-url-common.php
@@ -21,7 +21,6 @@ class Featured_Image_By_URL_Common {
 	public function __construct() {
 		
 		add_action( 'init', array( $this, 'knawatfibu_set_thumbnail_id_true' ) );
-		add_action( 'has_post_thumbnail', array( $this, 'knawatfibu_has_post_thumbnail_true' ), 20, 3 );
 		add_filter( 'post_thumbnail_html', array( $this, 'knawatfibu_overwrite_thumbnail_with_url' ), 999, 5 );
 		add_filter( 'woocommerce_structured_data_product', array( $this, 'knawatfibu_woo_structured_data_product_support' ), 99, 2 );
 		add_filter( 'facebook_for_woocommerce_integration_prepare_product', array( $this, 'knawatfibu_facebook_for_woocommerce_support' ), 99, 2 );
@@ -57,7 +56,7 @@ class Featured_Image_By_URL_Common {
 			$post_type = get_post_type( $product_id );
 			$image_data = $knawatfibu->admin->knawatfibu_get_image_meta( $product_id );
 			if ( isset( $image_data['img_url'] ) && $image_data['img_url'] != '' ){
-				return '_knawatfibu_fimage_url__' . $product_id;
+				return $product_id;
 			}
 		}
 		return $value;
@@ -101,36 +100,8 @@ class Featured_Image_By_URL_Common {
 						return $value;
 					}
 				}
-				return '_knawatfibu_fimage_url__' . $object_id;
+				return $object_id;
 			}
-		}
-		return $value;
-	}
-
-	/**
-	 * Set 'has_post_thumbnail' true if post has external featured image.
-	 *
-	 * @since 1.1
-	 */
-	function knawatfibu_has_post_thumbnail_true( $value, $object_id, $thumbnail_id ){
-
-		global $knawatfibu;
-		$post = get_post( $object_id );
-		$post_type = get_post_type( $post->ID );
-		if( $this->knawatfibu_is_disallow_posttype( $post_type ) ){
-			return $value;
-		}
-
-		$image_data = $knawatfibu->admin->knawatfibu_get_image_meta( $post->ID );
-		if ( isset( $image_data['img_url'] ) && $image_data['img_url'] != '' ){
-			if( $post_type == 'product_variation' ){
-				if( !is_admin() ){
-					return $post->ID;
-				}else{
-					return $value;
-				}
-			}
-			return '_knawatfibu_fimage_url__' . $post->ID;
 		}
 		return $value;
 	}
@@ -387,17 +358,8 @@ class Featured_Image_By_URL_Common {
 			}
 		}
 
-		$is_product_image = ( false !== strpos( $attachment_id, '_knawatfibu_fimage_url' ) );
-		$is_productvariation_image = ( is_numeric($attachment_id ) && $attachment_id > 0 && 'product_variation' == get_post_type( $attachment_id ) );
-		if( $is_product_image || $is_productvariation_image ){
-
-			$product_id = $attachment_id;
-			if( $is_product_image ){
-				$attachment = explode( '__', $attachment_id );
-				$product_id  = $attachment[1];
-			}
-
-			$image_data = $knawatfibu->admin->knawatfibu_get_image_meta( $product_id, true );
+		if( is_numeric($attachment_id ) && $attachment_id > 0 ){
+			$image_data = $knawatfibu->admin->knawatfibu_get_image_meta( $attachment_id, true );
 
 			// if( !empty( $image_data['img_url'] ) ){
 			if ( isset( $image_data['img_url'] ) && $image_data['img_url'] != '' ){
